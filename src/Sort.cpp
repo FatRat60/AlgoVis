263e12b05dfa71694eafd609ee0c0b9545913e2e
Kyle Hultgren <kylehultgren99@yahoo.com>
2024-03-22 00:35:02 -0700

    Refactored Shapes array to be a struct

--- src/Sort.cpp
+++ src/Sort.cpp
@@ -14,7 +14,8 @@ Sort::Sort(sf::Vector2u screenSize, int arraySize, sortAlgorithms algorithm_choi
 
     for (int i = 0; i < arraySize; i++)
     {
-        data[i] = i+1;
+        // only set num. RectangleShape will be init from num in shape_from_num()
+        shapes[i].num = i+1;
     }
     // shufle data
     shuffle(screenSize);
@@ -23,25 +24,36 @@ Sort::Sort(sf::Vector2u screenSize, int arraySize, sortAlgorithms algorithm_choi
 void Sort::shuffle(sf::Vector2u screenSize)
 {
     generateSeed();
-    std::shuffle(data.begin(), data.end(), std::default_random_engine(rng_seed));
+    std::shuffle(shapes.begin(), shapes.end(), std::default_random_engine(rng_seed));
 
     // update shapes
+    shape_from_num(screenSize);
+}
+
+void Sort::generateSeed()
+{
+    rng_seed = std::chrono::system_clock::now().time_since_epoch().count();
+}
+
+void swap(Shape& s1, Shape& s2)
+{
+    s1.swap(s2);
+}
+
+void Sort::shape_from_num(sf::Vector2u screenSize)
+{
     float heightInc = float(screenSize.y) / arraySize;
     float width = float(screenSize.x) / arraySize;
 
     // init shapes
     for (int i = 0; i < arraySize; i++)
     {
-        float height = heightInc * data[i];
-        shapes[i].setFillColor(sf::Color::White);
-        shapes[i].setSize(sf::Vector2f(width, height));
-        shapes[i].setPosition(sf::Vector2f(i * width, screenSize.y - height));
+        Shape *cur = &shapes[i];
+        float height = heightInc * cur->num;
+        cur->rect.setFillColor(sf::Color::White);
+        cur->rect.setSize(sf::Vector2f(width, height));
+        cur->rect.setPosition(sf::Vector2f(i * width, screenSize.y - height));
     }
-}
-
-void Sort::generateSeed()
-{
-    rng_seed = std::chrono::system_clock::now().time_since_epoch().count();
 }
 
 void Sort::doSort()
--- src/Sort.h
+++ src/Sort.h
@@ -3,6 +3,7 @@
 
 #include <SFML/Graphics.hpp>
 #include <array>
+#include <cstdint>
 
 #define MAX_ARRAY_SIZE 1000
 
@@ -11,11 +12,34 @@ enum sortAlgorithms
     bubble_sort
 };
 
+struct Shape 
+{
+    sf::RectangleShape rect;
+    uint16_t num;
+
+    // swap func based on num
+    void swap(Shape& other)
+    {
+        std::swap(num, other.num);
+    }
+
+    // overload for <
+    friend bool operator<(const Shape& s1, const Shape& s2)
+    {
+        return s1.num < s2.num;
+    }
+
+    // overload for ==
+    friend bool operator==(const Shape& s1, const Shape& s2)
+    {
+        return s1.num == s2.num;
+    }
+};
+
 class Sort
 {
     public:
-        std::array<uint16_t, MAX_ARRAY_SIZE> data;
-        std::array<sf::RectangleShape, MAX_ARRAY_SIZE> shapes;
+        std::array<Shape, MAX_ARRAY_SIZE> shapes;
         int arraySize;
         sortAlgorithms chosenAlgorithm;
         unsigned rng_seed;
@@ -23,6 +47,8 @@ class Sort
         void doSort();
         void shuffle(sf::Vector2u screenSize);
         void generateSeed();
+        friend void swap(Shape& s1, Shape& s2);
+        void shape_from_num(sf::Vector2u screenSize);
 
     private:
         void bubbleSort();
